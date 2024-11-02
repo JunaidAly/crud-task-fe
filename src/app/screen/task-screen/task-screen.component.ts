bd7dd5ccc9c091fa5389427551d995973bca2d29
JunaidAly <juanidalikhan03@gmail.com>
2024-11-02 16:58:07 +0500

    install angular project

---
+++ src/app/screen/task-screen/task-screen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-screen',
+  templateUrl: './task-screen.component.html',
+  styleUrl: './task-screen.component.scss'
+})
+export class TaskScreenComponent {
+
+}
